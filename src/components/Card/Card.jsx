6f8c73552afe781232e18d6c411ac4d884f96b81
kaliptoDev <baptiste.andre17@gmail.com>
2023-05-16 10:03:44 +0200

    Cards updated
    Cards updated in order to fit requierements. They now lead to a specific page dynamically.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,17 @@
 import './Card.css'
 
-const Card = ({ image, title }) => {
-
+const Card = ({ id, image, title}) => {
+    console.log(id)
     return (
-        <div className="card" style={image ? { backgroundImage: "url(" + image + ")", backgroundPosition: "center center", backgroundSize: "cover" } : null}>
-            <span className='cardTitle' >{title}</span>
-        </div >
+        <div className='card__container'>
+            <a href={`/${id}`} className='card__link'>
+            <div className='card__image_container'>
+                <div className="card" style={image ? { backgroundImage: "url(" + image + ")", backgroundPosition: "center center", backgroundSize: "cover" } : null}>
+                </div>
+            </div >
+            <h2 className='cardTitle' >{title}</h2>
+            </a>
+        </div>
     )
 
 }
