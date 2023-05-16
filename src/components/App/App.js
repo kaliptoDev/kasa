bdd04644d106491fda1131bc317253a60934da9f
kaliptoDev <baptiste.andre17@gmail.com>
2023-05-16 10:05:00 +0200

    Routes updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,15 @@
 import './App.css';
 import Header from '../Header/Header';
-import Tag from '../Tag/Tag';
-import Card from '../Card/Card';
-import Dropdown from '../Dropdowns/Dropdown/Dropdown';
+import RoutesTree from '../RoutesTree/RoutesTree';
+import Footer from '../Footer/Footer';
 
 
 const App = () => {
   return (
     <div className='main'>
-      <span className='componentDesc'>HEADER</span>
-    <Header />
-      <span className='componentDesc'>TAG</span>
-    <Tag text='tag-name' />
-      <span className='componentDesc'>CARD</span>
-    <Card title="Titre de la location" />
-      <span className='componentDesc'>SCROLL DESC</span>
-    <Dropdown text = "Description Test" list={["Test1","Test2","Test3","Test4"]}/>
+      <Header />
+      <RoutesTree />
+      <Footer />
     </div>
   );
 }
