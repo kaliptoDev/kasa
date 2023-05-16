969b7d2047c6fd48cc25dd360ece45dd92bc2c28
kaliptoDev <baptiste.andre17@gmail.com>
2023-05-16 10:04:06 +0200

    Routes has been started
    Routes started and updated, in progress.

---
+++ src/components/RoutesTree/RoutesTree.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import ErrorPage from '../ErrorPage/ErrorPage';
+import About from '../About/About';
+import Home from '../Home/Home';
+
+
+const RoutesTree = () => {
+
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' component={<ErrorPage /> } />
+        </Routes>
+    )
+
+}
+
+export default RoutesTree;
