a4dbe86fa08bab547e582486bf1e369e259f28f5
kaliptoDev <baptiste.andre17@gmail.com>
2023-05-24 13:10:58 +0200

    Logo in nav is a link now
    Added an <a> to the logo, so it takes you to "/"

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -4,7 +4,9 @@ import { ReactComponent as LogoSVG } from '../../../assets/Logo/logo.svg';
 const Logo = () => {
     return (
         <div className="logo">
+            <a href='/' title="Retourner à l'accueil">
             <LogoSVG className="logo__svg" />
+            </a>
         </div>
     );
 
