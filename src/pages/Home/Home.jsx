01afa964804e320eb1b4eb03499f15a4c46b7f22
kaliptoDev <baptiste.andre17@gmail.com>
2023-05-25 14:39:33 +0200

    Cleaned and fixed the code
    Cleaned comments
    Fixed the data import from a file to a useContext

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,46 +1,22 @@
 import './Home.css'
-// import data from '../../assets/Data/data.json'
 import Card from '../../components/Card/Card'
-import { useEffect, useState } from 'react'
-import ErrorPage from '../ErrorPage/ErrorPage'
+import { useContext, useEffect, useState } from 'react'
 import Loading from '../../components/Loading/Loading'
-import useFetchData from '../../utils/useFetchData'
+import { DataContext } from '../../Context/DataContext'
 
 const Home = () => {
+    
+    const [data, setData] = useState(null);
 
-    // useEffect(() => {
-    // try / catch
-    // fetch('../../assets/Data/data.json')
-    // }, [???])
-    let data = null
-
-    // const [data, setData] = useState(null);
-    // const [isLoading, setIsLoading] = useState(true);
-    // const { data, error, isLoaded } = useFetchData('./Data/data.json')
-    // const adress = 'src\assets\Data\data.json'
-
-    // useEffect( () => {
-    //     setIsLoading(true);
-    //     async function fetchData () {
-    //         try {
-    //         const reponse = await fetch('../../assets/Data/data.json')
-    //         const data = await reponse.json()
-    //         setData(data)
-    //         console.log(data)
-    //     } catch (error) {
-    //         alert('Erreur :' + error)
-    //         return <ErrorPage />
-    //     }
-    //     }
-    //     fetchData()
-        
-    //     setIsLoading(false);
-
-    // }, []);
-
+    const res = useContext(DataContext)
+    
     useEffect(() => {
         document.title = 'Kasa - Accueil'
     }, [])
+
+    useEffect(() => {
+        setData(res)
+    }, [res])
 
 
     return (
@@ -52,13 +28,12 @@ const Home = () => {
                     <h1 className='title'>Chez vous, partout et ailleurs</h1>
                 </div>
 
-
-
             </div>
             <div className='home__container__cards'>
-                {data? data.map((card, index) => {
-                    return <Card id={card.id} image={card.cover} title={card.title} key={index}/>
-                }) : <Loading />}
+                {data ? data.map((card, index) => {
+                    return <Card id={card.id} image={card.cover} title={card.title} key={index} fiche={card} />
+                }) : <Loading />
+                }
 
             </div>
 
