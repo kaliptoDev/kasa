eb716ca356df4c39c769da8b5089e91f54869c1b
kaliptoDev <baptiste.andre17@gmail.com>
2023-05-25 14:33:20 +0200

    Card component modified
    Changed the <a> to a <Link />

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,16 @@
+import { Link } from 'react-router-dom'
 import './Card.css'
 
 const Card = ({ id, image, title}) => {
+    
     return (
-        <div className='card__container'>
-            <a href={`/fiche-logement/${id}`} className='card__link'>
+        <div className='card__container'>                
             <div className='card__image_container'>
                 <div className="card" style={image ? { backgroundImage: "url(" + image + ")", backgroundPosition: "center center", backgroundSize: "cover" } : null}>
+                <Link to={`/fiche-logement/${id}`} className='card__link' />
                 </div>
             </div >
             <h2 className='cardTitle' >{title}</h2>
-            </a>
         </div>
     )
 
