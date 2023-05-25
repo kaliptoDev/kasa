d12e45ef6e8ef32298da5f03c92b995dce864468
kaliptoDev <baptiste.andre17@gmail.com>
2023-05-25 14:32:22 +0200

    Context Provider added
    Context provider added to provide data to childs withour prop drilling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,29 @@ import './App.css';
 import Header from '../Header/Header';
 import RoutesTree from '../../router/RoutesTree/RoutesTree';
 import Footer from '../Footer/Footer';
-
+import { DataContext } from '../../Context/DataContext'
+import useFetchData from '../../utils/useFetchData'
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  
+  const [data, setData] = useState(null);
+  
+  let res = useFetchData('./Data/data.json')
+  
+  console.log(data)
+
+  useEffect(() => {
+    setData(res.data)
+  }
+    , [res])
+
   return (
     <div className='main'>
       <Header />
-      <RoutesTree />
+      <DataContext.Provider value={data}>
+        <RoutesTree />
+      </DataContext.Provider>
       <Footer />
     </div>
   );
