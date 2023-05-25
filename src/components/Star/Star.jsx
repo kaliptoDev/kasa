9e69ac171f5e3cc0e041885b4a880f131faa5971
kaliptoDev <baptiste.andre17@gmail.com>
2023-05-25 15:12:30 +0200

    Removed comment

--- src/components/Star/Star.jsx
+++ src/components/Star/Star.jsx
@@ -2,7 +2,6 @@ import './Star.css'
 import { ReactComponent as StarSVG } from '../../assets/Star/star.svg'
 
 const Star = ({ isActive }) => {
-    console.log(isActive)
     return (
         <div className='star'>
             <StarSVG className={isActive ? 'star__active' : 'star__inactive'} />
