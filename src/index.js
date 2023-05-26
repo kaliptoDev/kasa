9e214bbb176e0ae6805bbdf2c73f39bedcbf1cec
kaliptoDev <baptiste.andre17@gmail.com>
2023-05-26 09:22:49 +0200

    Changed from DataContext to DataProvider

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import DataProvider from './providers/DataProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <DataProvider>
+      <Router>
+        <App />
+      </Router>
+    </DataProvider>
   </React.StrictMode>
 );
 
