c0ad60b49988e1e99f8ec38b46742b8cf241e91e
kaliptoDev <baptiste.andre17@gmail.com>
2023-05-26 10:29:36 +0200

    Removed useless imports

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,7 @@ import './App.css';
 import Header from '../Header/Header';
 import RoutesTree from '../../router/RoutesTree/RoutesTree';
 import Footer from '../Footer/Footer';
-import { DataContext } from '../../Context/DataContext'
-import useFetchData from '../../utils/useFetchData'
-import { useState, useEffect } from 'react';
+
 
 
 const App = () => {
